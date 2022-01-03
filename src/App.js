0ec2e9990b05a287d90fca0bc81d4e8ed7603c63
pappyJ <peterjoshua828@gmail.com>
2022-01-03 20:41:11 +0100

    spinnner fix v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Loader from "./components/loader";
-import Homepage from './pages';
+import Homepage from "./pages";
 
 function App() {
   const [loaded, setLoaded] = React.useState(true);
 
   React.useEffect(() => {
-  setTimeout(() => {
-    setLoaded(false)
+    setTimeout(() => {
+      setLoaded(false);
     }, 8000);
-  },[]);
-  return (
-    <div className="App">
-      {
-        loaded ? <Loader /> :
-          
-      <Homepage/>
-      }
-    </div>
-  );
+  }, []);
+  return <div className="App">{loaded ? <Loader /> : <Homepage />}</div>;
 }
 
 export default App;
